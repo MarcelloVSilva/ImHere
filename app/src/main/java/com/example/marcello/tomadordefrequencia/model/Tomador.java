8d663194cd2db4ee2a952e6fa18f827f6f0d3341
MarcelloVSilva <marcellovcs@gmail.com>
2018-06-11 02:01:24 -0300

    tentando ler do firebase

---
+++ app/src/main/java/com/example/marcello/tomadordefrequencia/model/Tomador.java
@@ -0,0 +1,22 @@
+package com.example.marcello.tomadordefrequencia.model;
+
+import java.lang.reflect.Array;
+
+/**
+ * Created by marcello on 6/10/18.
+ */
+
+public class Tomador {
+    private Array disciplinas;
+
+    public Tomador() {
+    }
+
+    public Array getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(Array disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+}
